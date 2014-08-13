1749cfec8769b5b88b6129f1d1168592c793478e
will.HPCC <lazydomino@163.com>
2014-08-13 15:57:56 +0800

    modify query_device function, and know port 1 is active.

--- pingpong-common.cpp
+++ pingpong-common.cpp
@@ -4,6 +4,7 @@ void query_device(Arguments* args) {
 	struct ibv_context *ctx;
 	struct ibv_device **device_list;
 	struct ibv_device_attr device_attr;
+	struct ibv_port_attr port_attr;
 	int device_num;
 
 	device_list = ibv_get_device_list(&device_num);
@@ -20,12 +21,30 @@ void query_device(Arguments* args) {
 	if (ibv_query_device(ctx, &device_attr)) {
 		throw std::runtime_error("ibv_query_device failed!");
 	}
+	if(ibv_query_port(ctx, 1, &port_attr)){
+		throw std::runtime_error("ibv_query_port failed!");
+	}
 
 	printf("There are %d devices!\n", device_num);
 	printf("The device '%s' max mr size is %u bytes.\n",
 			ibv_get_device_name(ctx->device), device_attr.max_mr_size);
 	printf("The device '%s' max_qp_wr is %u.\n",
 			ibv_get_device_name(ctx->device), device_attr.max_qp_wr);
+
+	printf("The device '%s' phys_port_cnt is %u.\n",
+				ibv_get_device_name(ctx->device), device_attr.phys_port_cnt);
+
+	printf("The device '%s' active_speed is %s.\n",
+				ibv_get_device_name(ctx->device), active_speed[port_attr.active_speed]);
+	printf("The device '%s' max_mtu is %s.\n",
+				ibv_get_device_name(ctx->device), mtu[port_attr.max_mtu]);
+	printf("The device '%s' active_mtu is %s.\n",
+					ibv_get_device_name(ctx->device), mtu[port_attr.active_mtu]);
+
+	printf("The device '%s' link_layer is %s.\n",
+						ibv_get_device_name(ctx->device), link_layer[port_attr.link_layer]);
+
+
 
 	args->max_mr_size = device_attr.max_mr_size;
 	args->max_qp_wr = device_attr.max_qp_wr;
--- pingpong-common.h
+++ pingpong-common.h
@@ -12,18 +12,27 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <stdint.h>
-
+#include <map>
 #include "Arguments.h"
 
 #define RESOLVE_TIMEOUT_MS	5000
 #define MAXBUFFERSIZE	(1024*1024*256) // 1GByte
-
 
 struct pdata {
 	uint64_t buf_va;
 	uint32_t buf_rkey;
 };
 
+std::map<uint8_t, const char*> active_speed = { { 1, "2.5 Gbps" }, { 2,
+		"5.0 Gbps" }, { 4, "10.0 Gbps" }, { 8, "10.0 Gbps" },
+		{ 16, "14.0 Gbps" }, { 32, "25.0 Gbps" } };
+
+std::map<int, const char*> mtu = { { 1, "IBV_MTU_256" }, { 2, "IBV_MTU_512" }, {
+		3, "IBV_MTU_1024" }, { 4, "IBV_MTU_2048" }, { 5, "IBV_MTU_4096" } };
+std::map<uint8_t, const char*> link_layer = { { IBV_LINK_LAYER_UNSPECIFIED,
+		"IBV_LINK_LAYER_UNSPECIFIED" }, { IBV_LINK_LAYER_INFINIBAND,
+		"IBV_LINK_LAYER_INFINIBAND" }, { IBV_LINK_LAYER_ETHERNET,
+		"IBV_LINK_LAYER_ETHERNET" } };
 void query_device(Arguments* args);
 
 #endif /* PINGPING_COMMON_H_ */
